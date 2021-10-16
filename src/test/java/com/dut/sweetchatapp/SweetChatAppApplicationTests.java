612ed567eb655b01fdff71f44b6e600a7e394dbf
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2021-10-16 13:32:32 +0700

    first commit

---
+++ src/test/java/com/dut/sweetchatapp/SweetChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dut.sweetchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
